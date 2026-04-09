e25047502f668e9521e17a3def6171cfe19a98e2
Abhishekgupta501 <abhiapolo501@gmail.com>
2026-04-09 12:10:37 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,16 @@
+# name=input("enter your name: ")
+# age=int(input("enter your age: "))      
+# print("your name is: ",name)
+# print("your age is: ",age)
+# print("your name is: ",name,"and your age is: ",age)
+
+
+
+
+Class_Student=["Chandan","Satvik","Aman"]
+Class_Student[0]="Chandan Kumar"
+print(Class_Student[0])
+print("the students in the class are: ",Class_Student)
+print(len(Class_Student))                 #total number of element nikaal sakte hai list me 
+reversed_Class_Student=Class_Student[::-1]
+print("the students in the class in reverse order are: ",reversed_Class_Student)
